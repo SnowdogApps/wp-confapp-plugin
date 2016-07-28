d5b37e9f456531ff6066db80f7cc06d57e58497a
Bartek Igielski <igloczek@gmail.com>
2016-07-28 23:05:24 +0200

    Working copy of template

--- confapp/confappTemplate.php
+++ confapp/confappTemplate.php
@@ -3,157 +3,124 @@
  Template Name: ConfApp Template
  */
 ?>
-<?php
-get_header();
-?>
 
-<?php
-$_confrence_days = getConfrenceDays();
+<?php get_header(); ?>
 
-?>
 <div id="primary" class="content-areas">
     <main id="main" class="site-main" role="main">
 
         <?php
-        // Start the loop.
-        while (have_posts()) : the_post();
+          // Start the loop.
+          while (have_posts()) : the_post();
 
-            // Include the page content template.
-            get_template_part('template-parts/content', 'page');
+              // Include the page content template.
+              get_template_part('template-parts/content', 'page');
 
-            // If comments are open or we have at least one comment, load up the comment template.
-            if (comments_open() || get_comments_number()) {
-                comments_template();
-            }
+              // If comments are open or we have at least one comment, load up the comment template.
+              if (comments_open() || get_comments_number()) {
+                  comments_template();
+              }
 
-            // End of the loop.
-        endwhile;
+              // End of the loop.
+          endwhile;
         ?>
 
-        <ul class="conf-days">
-            <?php foreach ($_confrence_days as $_confrence_day): ?>
-                <li class="conf-days__item conf-days__item--selected" data-filter="19/09">
-                    <div class="conf-days__day"><?php echo $_confrence_day->name ?></div>
-                </li>
-            <?php endforeach; ?>
+        <?php
+          $days = getConfrenceDays();
+          $tracks = getConfrenceTracks();
+          $localizations = getConfrenceLocalizations();
+
+          function getTrackName($tracks, $trackId) {
+            foreach ($tracks as $track) {
+              return $track->id === $trackId ? $track->name : false;
+            }
+          }
+        ?>
+
+
+        <ul class="conf-tracks">
+          <li class="conf-tracks__item conf-tracks__item--selected" data-filter="all">
+            All tracks
+          </li>
+          <?php foreach ($tracks as $track): ?>
+            <li class="conf-tracks__item" data-track-filter="<?= $track->id ?>">
+              <?= $track->name ?>
+            </li>
+          <?php endforeach; ?>
         </ul>
+
         <ul class="conf-room">
-            <li class="conf-room__item" data-filter="Open Space">Open Space</li>
-            <li class="conf-room__item" data-filter="Creative Room">Creative Room</li>
-            <li class="conf-room__item" data-filter="Devs Room">Devs Room</li>
-            <li class="conf-room__item conf-room__item--selected" data-filter="all">All</li>
-        </ul>
-        <ul class="conf-agenda conf-agenda--active" data-day="19/09">
-            <li class="conf-agenda__item">
-                <div class="conf-agenda__row" data-room="Devs Room">
-                    <div class="conf-agenda__hour">19:00</div>
-                    <div class="conf-agenda__presentation">
-                        <div class="conf-agenda__presentation-subject">Magento 2</div>
-                        <div class="conf-agenda__presentation-speaker">Ben Marks</div>
-                    </div>
-                    <div class="conf-agenda__info">
-                        <div class="conf-agenda__language flag flag--pl"></div>
-                        <div class="conf-agenda__room-type">Devs Room</div>
-                    </div>
-                </div>
-                <div class="conf-agenda__row" data-room="Open Space">
-                    <div class="conf-agenda__hour">19:00</div>
-                    <div class="conf-agenda__presentation">
-                        <div class="conf-agenda__presentation-subject">Magento 2</div>
-                        <div class="conf-agenda__presentation-speaker">Ben Marks</div>
-                    </div>
-                    <div class="conf-agenda__info">
-                        <div class="conf-agenda__language flag flag--gb"></div>
-                        <div class="conf-agenda__room-type">Open Space</div>
-                    </div>
-                </div>
+          <li class="conf-room__item conf-room__item--selected" data-room-filter="all">
+            All rooms
+          </li>
+          <?php foreach ($localizations as $localization): ?>
+            <li class="conf-room__item" data-room-filter="<?= $localization->id ?>">
+              <?= $localization->name ?>
             </li>
-            <li class="conf-agenda__item">
-                <div class="conf-agenda__row" data-room="Devs Room">
-                    <div class="conf-agenda__hour">21:00</div>
-                    <div class="conf-agenda__presentation">
-                        <div class="conf-agenda__presentation-subject">Magento 2</div>
-                        <div class="conf-agenda__presentation-speaker">Ben Marks</div>
-                    </div>
-                    <div class="conf-agenda__info">
-                        <div class="conf-agenda__language flag flag--pl"></div>
-                        <div class="conf-agenda__room-type">Devs Room</div>
-                    </div>
-                </div>
-                <div class="conf-agenda__row" data-room="Open Space">
-                    <div class="conf-agenda__hour">21:00</div>
-                    <div class="conf-agenda__presentation">
-                        <div class="conf-agenda__presentation-subject">Magento 2</div>
-                        <div class="conf-agenda__presentation-speaker">Ben Marks</div>
-                    </div>
-                    <div class="conf-agenda__info">
-                        <div class="conf-agenda__language flag flag--pl"></div>
-                        <div class="conf-agenda__room-type">Open Space</div>
-                    </div>
-                </div>
-            </li>
-        </ul>
-        <ul class="conf-agenda conf-agenda--active" data-day="20/09">
-            <li class="conf-agenda__item">
-                <div class="conf-agenda__row" data-room="Devs Room">
-                    <div class="conf-agenda__hour">19:00</div>
-                    <div class="conf-agenda__presentation">
-                        <div class="conf-agenda__presentation-subject">Magento 2</div>
-                        <div class="conf-agenda__presentation-speaker">Ben Marks</div>
-                    </div>
-                    <div class="conf-agenda__info">
-                        <div class="conf-agenda__language flag flag--gb"></div>
-                        <div class="conf-agenda__room-type">Devs Room</div>
-                    </div>
-                </div>
-                <div class="conf-agenda__row" data-room="Open Space">
-                    <div class="conf-agenda__hour">19:00</div>
-                    <div class="conf-agenda__presentation">
-                        <div class="conf-agenda__presentation-subject">Magento 2</div>
-                        <div class="conf-agenda__presentation-speaker">Ben Marks</div>
-                    </div>
-                    <div class="conf-agenda__info">
-                        <div class="conf-agenda__language flag flag--pl"></div>
-                        <div class="conf-agenda__room-type">Open Space</div>
-                    </div>
-                </div>
-            </li>
-            <li class="conf-agenda__item">
-                <div class="conf-agenda__row" data-room="Devs Room">
-                    <div class="conf-agenda__hour">21:00</div>
-                    <div class="conf-agenda__presentation">
-                        <div class="conf-agenda__presentation-subject">Magento 2</div>
-                        <div class="conf-agenda__presentation-speaker">Ben Marks</div>
-                    </div>
-                    <div class="conf-agenda__info">
-                        <div class="conf-agenda__language flag flag--gb"></div>
-                        <div class="conf-agenda__room-type">Devs Room</div>
-                    </div>
-                </div>
-                <div class="conf-agenda__row" data-room="Open Space">
-                    <div class="conf-agenda__hour">21:00</div>
-                    <div class="conf-agenda__presentation">
-                        <div class="conf-agenda__presentation-subject">Magento 2</div>
-                        <div class="conf-agenda__presentation-speaker">Ben Marks</div>
-                    </div>
-                    <div class="conf-agenda__info">
-                        <div class="conf-agenda__language flag flag--pl"></div>
-                        <div class="conf-agenda__room-type">Open Space</div>
-                    </div>
-                </div>
-            </li>
+          <?php endforeach; ?>
         </ul>
 
+        <div class="conf-days">
+          <?php foreach ($days as $day): ?>
+            <button class="conf-days__item" data-day-filter="<?= $day->date; ?>">
+              <?= $day->date; ?> / <?= $day->name; ?>
+            </button>
+          <?php endforeach; ?>
+        </div>
 
+        <?php foreach ($days as $day): ?>
+          <ul class="conf-agenda" data-day="<?= $day->date ?>">
+            <?php $presentationsByDate = []; ?>
+            <?php
+              foreach (getConfrencePresentations($day->id) as $presentation) {
+                if ($presentationsByDate[$presentation->date]) {
+                  array_push($presentationsByDate[$presentation->date], $presentation);
+                }
+                else {
+                  $presentationsByDate[$presentation->date] = [$presentation];
+                }
+              }
+            ?>
+            <?php ksort($presentationsByDate); ?>
 
+            <?php foreach ($presentationsByDate as $date => $presentationsInSameTime): ?>
+              <li class="conf-agenda__item">
+                <?php foreach ($presentationsInSameTime as $key => $presentation): ?>
+                  <pre>
+                    <?php print_r($presentation); ?>
+                  </pre>
 
-        getConfrenceData: <?php var_dump(getConfrenceData()); ?><br/>
-        getConfrenceDays: <?php var_dump(getConfrenceDays()); ?><br/>
-        getConfrenceFloors: <?php var_dump(getConfrenceFloors()); ?><br/>
-        getConfrencePresentations: <?php var_dump(getConfrencePresentations($dayId, $trackId)); ?><br/>
-        getSpeaker: <?php var_dump(getSpeaker($speakerId)); ?><br/>
-        getTracks: <?php var_dump(getTracks()); ?><br/>
+                  <div class="conf-agenda__row"
+                       data-room=""
+                       data-track="<?= $presentation->track_id ?>"
+                  >
+                      <div class="conf-agenda__hour">
+                        <?= date_format(date_create($date), 'H:i'); ?>
+                      </div>
+                      <div class="conf-agenda__presentation">
+                          <div class="conf-agenda__presentation-subject">
+                            Magento 2
+                          </div>
+                          <div class="conf-agenda__presentation-speaker">
+                            Ben Marks
+                          </div>
+                      </div>
+                      <div class="conf-agenda__info">
+                          <div class="conf-agenda__language conf-agenda__language--<?= $presentation->locale ?>"></div>
+                          <div class="conf-agenda__room-type">
+                            <?= $presentation->localization_id ?>
+                          </div>
 
+                          <div class="conf-agenda__track-type">
+                            <?= getTrackName($tracks, $presentation->track_id) ?>
+                          </div>
+                      </div>
+                  </div>
+                <?php endforeach; ?>
+              </li>
+            <?php endforeach; ?>
+          </ul>
+        <?php endforeach; ?>
 
     </main><!-- .site-main -->
 </div><!-- .content-area -->
